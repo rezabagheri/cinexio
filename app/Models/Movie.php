fdd28b1e678b334ba1e3fb11727886d48a0d3c44
Rezo <rezabagheri@gmail.com>
2025-05-20 09:10:54 +0400

    Add Movies model and table

---
+++ app/Models/Movie.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Cinexio - A personal movie and series archive management system with social networking features.
+ *
+ * This file is part of the Cinexio project, a free software for managing and sharing movie archives.
+ *
+ * @package Cinexio
+ * @author Reza Bagheri <rezabagheri@gmail.com>
+ * @copyright 2025 Reza Bagheri
+ * @license MIT License
+ * @version 1.0.0
+ * @link https://github.com/rezabagheri/cinexio
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * Class Movie
+ *
+ * Represents a movie entity in the Cinexio system.
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $original_title
+ * @property int|null $release_year
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
+class Movie extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'original_title',
+        'release_year',
+        'description',
+    ];
+
+    /**
+     * Get the movie versions associated with this movie.
+     *
+     * @return HasMany
+     */
+    public function versions(): HasMany
+    {
+        return $this->hasMany(MovieVersion::class);
+    }
+}
